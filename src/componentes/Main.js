0861e3135de62c1bb5546a295091411a5a305c96
Euderlan <euderlanfreire@gmail.com>
2025-01-22 23:26:53 -0300

    vendo sei fazer commit

---
+++ src/componentes/Main.js
@@ -0,0 +1,13 @@
+import style from "./Main.module.css";
+
+function Main(){
+    return(
+        
+        <div className={style.Main_container}>
+            <h1>Lista de Tarefas</h1>
+
+        </div>
+    )
+}
+
+export default Main;
